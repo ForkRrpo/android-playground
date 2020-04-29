0cf687ae8549bb542de0b7284eaf4f0dbf600af2
Husayn Hakeem <husaynhakeem@gmail.com>
2020-04-28 22:21:19 -0700

    Create MergeAdapter cases Activities
    Create Activities for the different cases MergeAdapter can be used in, and connect them to the MainActivity.

---
+++ MergeAdapterSample/app/src/main/java/com/husaynhakeem/mergeadaptersample/DifferentViewHoldersActivity.kt
@@ -0,0 +1,7 @@
+package com.husaynhakeem.mergeadaptersample
+
+import androidx.appcompat.app.AppCompatActivity
+
+class DifferentViewHoldersActivity: AppCompatActivity() {
+
+}
--- MergeAdapterSample/app/src/main/java/com/husaynhakeem/mergeadaptersample/MainActivity.kt
+++ MergeAdapterSample/app/src/main/java/com/husaynhakeem/mergeadaptersample/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.husaynhakeem.mergeadaptersample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        differentViewHolders.setOnClickListener {
+            startActivity(Intent(this, DifferentViewHoldersActivity::class.java))
+        }
+
+        sameViewHolders.setOnClickListener {
+            startActivity(Intent(this, SameViewHoldersActivity::class.java))
+        }
+
+        complexViewHolders.setOnClickListener {
+            startActivity(Intent(this, ComplexViewHoldersActivity::class.java))
+        }
     }
 }
